f075666348cd7475c870bf698d4ed9bbc0646fa2
k-furusho <k.furusho@bbtech.jp>
2022-09-21 23:59:24 +0900

    add logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -21,10 +21,29 @@ export class AuthController {
   signUp(@Body() dto: AuthDTO): Promise<Msg> {
     return this.authService.signUp(dto)
   }
+
   @HttpCode(HttpStatus.OK)
   @Post('login')
   async login(@Body() dto: AuthDTO, @Res({ passthrough: true }) res: Response) {
     const jwt = await this.authService.login(dto)
+    res.cookie('access_token', jwt.accessToken, {
+      httpOnly: true,
+      secure: false, //NOTE:Postman動作確認のためにfalseにしてる
+      sameSite: 'none',
+      path: '/',
+    })
+    return {
+      message: 'success',
+    }
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('logout')
+  async logout(
+    @Body() dto: AuthDTO,
+    @Res({ passthrough: true }) res: Response
+  ) {
+    const jwt = await this.authService.logout(dto)
     res.cookie('access_token', jwt.accessToken, {
       httpOnly: true,
       secure: false, //NOTE:Postman動作確認のためにfalseにしてる
